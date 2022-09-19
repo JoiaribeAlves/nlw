ca0065e2c3bae11b3bfb187b1fb4c2f01f7f750d
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-09-19 00:13:56 -0400

    Configura o cors e importa as rotas

--- eSports/backend/src/server.ts
+++ eSports/backend/src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import cors from "cors";
+
+import { router } from "./routes";
 
 const port = 4000;
 const app = express();
 
 // Middlewares
 app.use(express.json());
-
-app.get("/ads", (req, res) => {
-	return res.json({ message: "Hello" });
-});
+app.use(
+	cors({
+		origin: "http://localhost:4000",
+	})
+);
+app.use(router);
 
 app.listen(port, () => console.log("Server is runnig."));
