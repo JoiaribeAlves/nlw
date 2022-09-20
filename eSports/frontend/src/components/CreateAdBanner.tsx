692c92e21b7690b2d823417b308d0519b7928342
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-09-20 18:30:40 -0400

    Cria o componente CreateBanner

---
+++ eSports/frontend/src/components/CreateAdBanner.tsx
@@ -0,0 +1,27 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
+
+export function CreateAdBanner() {
+  return (
+    <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+        <div>
+          <strong className="text-2xl text-white font-black">
+            Não encontrou seu duo?
+          </strong>
+          <p className="text-zinc-400">
+            Publique um anúncio para encontrar novos players!
+          </p>
+        </div>
+
+        <Dialog.Trigger
+          type="button"
+          className="flex items-center gap-3 py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded transition-colors"
+        >
+          <MagnifyingGlassPlus size={24} />
+          Publicar anúncio
+        </Dialog.Trigger>
+      </div>
+    </div>
+  );
+}
